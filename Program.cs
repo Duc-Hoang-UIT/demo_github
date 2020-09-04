f4be7c5008e880dcd1fa47cb7f0cd53dcc42e1fa
PRID021 <Jad410.00@gmail.com>
2020-09-04 09:09:53 +0700

    Demo git

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Let_get_started
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
